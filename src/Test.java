694619ff97260d66cb1a847cf3a010eefc6a836d
jaseongKim <jaseong4281@gmail.com>
2023-08-22 16:56:54 +0900

    test

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// test
-
-		String s = "dd";
-		// test//sssss
-		
-		
-		//test
+	
+		//진짜 마지막 테스트
 		
 	}
 
