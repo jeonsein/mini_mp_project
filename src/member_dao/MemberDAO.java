4377b05efcecc157a95189468fbd0bdf372ee64f
juntekc <125155263+juntekc@users.noreply.github.com>
2023-08-25 12:35:14 +0900

    [준택] 마이페이지 작성중

--- src/member_dao/MemberDAO.java
+++ src/member_dao/MemberDAO.java
@@ -4,10 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import db.DBConnect;
 	
 public class MemberDAO {
 	private Connection conn;
 	private PreparedStatement pstmt = null;
+	
+	public MemberDAO() {
+		DBConnect dbConn = DBConnect.getInstance();
+		conn = dbConn.getConnection();
+	}
 	
 	public void close(ResultSet rs) {
 		if(rs != null) {
@@ -31,13 +38,14 @@ public class MemberDAO {
 			}
 		}
 	}
+	//회원정보를 불러오는 메소드
 	
-	public void ShowMember(String id) {
+	public void ShowMember(String id) { 
 		ResultSet rs=null;
 		
 		String SQL ="SELECT ID,TEL,NAME,REGION_CODE"
 				+ "FROM MEMBER"
-				+ "WHERE ID= '?' ";
+				+ "WHERE ID= ? ";
 		try {
 			pstmt =conn.prepareStatement(SQL);
 			pstmt.setString(1,id);
@@ -50,35 +58,89 @@ public class MemberDAO {
 		return;
 	}
 	
-	
+	//회원 탈퇴 메소드
 	public void DeleteMember(String id) {
 			 
-	ResultSet rs=null;
-	
-	String SQL = "DELETE MEMBER WHERE ID='?'; ";
-	
-	try {
-		pstmt =conn.prepareStatement(SQL);
-		pstmt.setString(1, SQL);
-		rs=pstmt.executeQuery();
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}finally {
+		String SQL = "DELETE MEMBER WHERE ID=? ";
 		
-	}
-		return;
+		try {
+			pstmt =conn.prepareStatement(SQL);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			
+		}
+			return;
+		}	 
+		
 	
-	}	 
 	
-	public void ShowWishList() {
+	//찜한 목록 불러오기 
+	public void ShowWishList(String id) {
 		ResultSet rs=null;
 		
 		String SQL = "SELECT m.id \"member의 id\", w.res_id \"wish_list의 식당번호\", r.res_name \"restaurant의 식당이름\", r.res_id \"restaurant의 식당번호\"\r\n"
 				+ "FROM member m JOIN wish_list w ON (m.id = w.id)\r\n"
 				+ "JOIN restaurant r ON (w.res_id = r.res_id)\r\n"
-				+ "WHERE m.id = '?'";
+				+ "WHERE m.id = ?" ; 
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	//비밀번호 변경  메소드 
+	public void UpdatePwd(String id,String pwd){
+		
+		String SQL="UPDATE MEMBER SET PWD=? WHERE ID=?";
+				
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
 	}
 
-
+	//이름 변경 메소드
+	public void UpdateName(String id, String name) {
+		
+		String SQL="UPDATE MEMBER SET NAME=? WHERE ID=?";
+		
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1,id);
+			pstmt.setString(2, name);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	//번호 변경 메소드
+	public void UpdateTel(String id, String region_code) {
+		
+		String SQL="UPDATE MEMBER SET REGION_ID=? WHERE ID=?"; 
+		
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1,id);
+			pstmt.setString(2,region_code);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
+	}
 
 }
--- src/pro.java
+++ src/pro.java
@@ -3,7 +3,7 @@ import member_dao.MemberDAO;
 public class pro {
 	
 	public static void main(String[] args) {
-
+		MemberDAO dao=new MemberDAO();
 		// 준택
 		
 		Scanner sc = new Scanner(System.in);
@@ -17,22 +17,41 @@ public class pro {
 			
 			//뒤로 가기 
 			if (opt.equals("0")) {
-				 break;
+				 
+				break;
 				
-			// 이름 변경
+			// 비밀번호변경
 			} else if (opt.equals("1")) {
-			 
+				System.out.println("바꿀 비밀번호를 입력하세요");
 				
-			// 전화번호 변경	
+				dao.UpdatePwd("joon", "111");
+			
+				//이름 변경 
 			} else if (opt.equals("2")) {
+				System.out.println("바꿀 아이디를 입력하세요");
 				
+				dao.UpdateName( "joon","mang1" );
 				
-			// 주소변경(시/도 변경만 가능)	
+				System.out.println("이름이 성공적으로 변경되었습니다.");
+				
+			// 전화번호 변경
 			} else if (opt.equals("3")) {
 			
-				 
+				
+			
+				
+			// 주소 변경(시/도 변경만 가능)	
 			} else if (opt.equals("4")) {
-		
+				System.out.println("지역번호를 선택해주세요."
+						+ "1.서울/경기/인천 "
+						+ "2.강원도"
+						+ "3.충청도"
+						+ "4.경상도"
+						+ "5.전라도"
+						+ "6.제주도" );
+				
+				String area =sc.next();
+				 
 				
 			}
 			break;
@@ -64,7 +83,7 @@ public class pro {
 			System.out.println("찜한 식당 확인하기 : 뒤로가기 - 0, 찜 삭제 - 1");
 		
 			String opt2 = sc.nextLine();
-			// 비번 변경 
+			
 			if (opt2.equals("0")) {
 			 
 				// 찜삭제
