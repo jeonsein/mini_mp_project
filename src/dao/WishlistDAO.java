6b436fc9d44cde8057a9bee746306e3dee53ce06
juntekc <125155263+juntekc@users.noreply.github.com>
2023-08-29 18:54:47 +0900

    [준택] 코드 합치는 중Q

--- src/dao/WishlistDAO.java
+++ src/dao/WishlistDAO.java
@@ -8,10 +8,6 @@ import java.sql.ResultSet;
 import dto.WishlistDTO;
 
 
-<<<<<<< HEAD:src/wishlist_dao/WishlistDAO.java
-//@Log4j2
-=======
->>>>>>> ee73766267e1f2d4826f869a80de700e41c7df8d:src/dao/WishlistDAO.java
 public class WishlistDAO implements WishlistDAOInterface {
 	// 찜하기 기능의 인터페이스를 정의함!
 
