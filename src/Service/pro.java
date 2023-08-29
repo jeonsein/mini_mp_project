9cbc9279ef2a89e3bd4d6a439a3af94b94db3be6
juntekc <125155263+juntekc@users.noreply.github.com>
2023-08-29 18:50:25 +0900

    [준택] 코드 합치는 중

--- src/Service/pro.java
+++ src/Service/pro.java
@@ -1,4 +1,4 @@
-package service;
+package Service;
 
 import java.util.Scanner;
 
@@ -91,13 +91,13 @@ public class pro {
 				System.out.println("======찜한 식당======");
 
 				if (opt1.equals("0")) {
-					dao.ShowWishList("mango");
+					dao.ShowWishList("jea");
 					System.out.println("불러오기 성공");
 				}
 				if (opt1.equals("1")) {
 					System.out.println("삭제할 레스토랑 아이디를 적으십시오.");
 					String opt2 = sc.nextLine();
-					dao.DeleteWishList(opt2, "mango");
+					dao.DeleteWishList(opt2, "jea");
 					System.out.println("찜삭제 성공");
 				}
 			} else {
