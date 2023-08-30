ab2243ffa767ad753aa3f3a8be46044e706abd5f
sengna <jeonseino_o@naver.com>
2023-08-31 01:03:24 +0900

    [셍나] 찜하기 수정

--- src/service/RestaurantService.java
+++ src/service/RestaurantService.java
@@ -16,20 +16,24 @@ import dto.WishlistDTO;
 public class RestaurantService {
 	
 	Scanner sc = new Scanner(System.in);
-		
+	
+	// DAO -> 데이터베이스와 상호작용을 담당
 	RestaurantDAO resDAO = new RestaurantDAO();
-	RestaurantDTO resDTO = new RestaurantDTO();
-	
 	RegionDAO regionDAO = new RegionDAO();
+	WishlistDAO wishlistDAO;
 	
+	// 로그인한 사용자의 정보를 저장
+	MemberDTO memberDTO;
+	
+	// DTO -> 데이터를 전달하는 데 사용
+	RestaurantDTO resDTO = new RestaurantDTO();
 	WishlistDTO wishlistDTO = new WishlistDTO();
 	
+	// 식당 & 지역 정보를 담는 리스트
 	List<RestaurantDTO> Restaurantlist = new ArrayList<>();
 	List<RegionDTO> regionList = new ArrayList<>();
 
-	WishlistDAO wishlistDAO;
-	MemberDTO memberDTO;
-	
+	// 생성자로 로그인한 사용자의 정보를 받아와 초기화
     public RestaurantService(MemberDTO loggedInUserId) {
         this.memberDTO = loggedInUserId;
         this.wishlistDAO = new WishlistDAO(loggedInUserId);  // WishlistDAO에 로그인한 사용자 정보 전달
@@ -40,7 +44,14 @@ public class RestaurantService {
 			System.out.println("");
 		}
 	}
+
+//	----------------------------------------------------
 	
+	/*
+	 * 사용자에게 맛집 리스트를 어떻게 보고 싶은지 선택하게 함.
+	 * 지역별로 보기 선택 시 regionList() 메소드를 호출!
+	 * 찜 많은 순으로 보기 선택 시 wishBestSelect() 메소드를 호출!
+	 */
 	// 맛집 리스트 보기
 	public void restaurantList() {
 		
@@ -64,6 +75,11 @@ public class RestaurantService {
 		
 	} // end of restaurantList
 	
+//	----------------------------------------------------
+	
+	/*
+	 * 사용자에게 지역 목록을 보여주고, 원하는 지역을 선택하게 한 후 regionSelect(int RegionNum) 메소드를 호출.
+	 */
 	// 지역 리스트 보기
 	public void regionList() {
 		printSpace();
@@ -76,6 +92,10 @@ public class RestaurantService {
 		regionSelect(select);
 	}
 	
+	/*
+	 * 선택한 지역의 식당 목록을 보여줌.
+	 * 사용자가 원하는 식당을 선택하면 selectRegionRes() 메소드를 호출하고, 찜하기를 선택하면 Wish_list에 추가합니다.
+	 */
 	// 지역 별로 보기
 	public void regionSelect(int RegionNum) {
 		printSpace();
@@ -89,10 +109,12 @@ public class RestaurantService {
 		}
 		System.out.print("원하시는 식당을 선택해주세요 : ");
 		int select = sc.nextInt();
+		// 사용자가 지역별로 정렬된 식당 목록에서 하나를 선택 -> 해당 입력값은 변수 n에 저장
 		int n = select;
 		
 		while(true) {
 			printSpace();
+			// selectRegionRes() 메소드 호출 -> 선택한 식당의 정보와 메뉴를 출력
 			selectRegionRes(regionNum, n);
 			System.out.println("원하시는 기능을 선택해주세요.");
 			System.out.println("0. 이전 페이지로 돌아가기");
@@ -104,15 +126,20 @@ public class RestaurantService {
 			
 			} else if(select == 1) {
 				
+				// 선택한 식당의 아이디를 가져옴!
 			    String wish_res_id = resDAO.selectRegionRes(regionNum, n).getRes_id();
 			    
 			    WishlistDTO wishlistDTO = new WishlistDTO();
 			    
+			    // // 로그인한 사용자의 id & 식당의 res_id를 wishlistDTO에 설정!
 			    wishlistDTO.setId(memberDTO.getId());
 			    wishlistDTO.setRes_id(wish_res_id);
 			    
 			    try {
 			    	
+			    	// 사용자가 이미 찜한 식당인지 확인하는 메소드.
+			    	// 이미 찜한 경우, "찜 목록에 이미 등록되어 있습니다!" 메시지 출력!
+			    	// 아니면, wishlistDAO.addWish(memberDTO.getId(), wish_res_id);를 호출 -> 찜 리스트에 추가
 			        if (wishlistDAO.isAlreadyWished(wishlistDTO)) {
 			            System.out.println("찜 목록에 이미 등록되어 있습니다!");
 			        } else {
@@ -132,6 +159,9 @@ public class RestaurantService {
 		} // while
 	} // regionSelect()
 	
+	/*
+	 * 선택한 지역과 식당에 따른 식당 정보와 메뉴를 출력.
+	 */
 	// 지역 순에서 원하는 식당 보기
 	public void selectRegionRes(int RegionNum, int selectNum) {
 		printSpace();
@@ -148,6 +178,13 @@ public class RestaurantService {
 		System.out.println(resDTO.getRes_info()+"\n");
 	}
 	
+//	----------------------------------------------------
+	
+	/*
+	 * 찜이 많이 된 식당순으로 보여줌
+	 * 사용자가 원하는 식당을 선택하면 selectWishRes() 메소드를 호출.
+	 * 찜하기를 선택하면 Wishl_ist에 추가!
+	 */
 	// 찜 많은 순으로 보기
 	public void wishBestSelect() {
 		printSpace();
@@ -159,9 +196,11 @@ public class RestaurantService {
 		}
 		System.out.print("원하시는 식당을 선택해주세요 : ");
 		int select = sc.nextInt();
+		// 사용자가 찜 많은 순으로 정렬된 식당 목록에서 하나를 선택 후, 해당 입력값을 wish_n에 저장함!
 		int wish_n = select;
 		while(true) {
 			printSpace();
+			// selectWishRes(wish_n) 메소드 호출 -> 사용자가 선택한 식당의 정보와 메뉴를 출력.
 			selectWishRes(wish_n);
 			System.out.println("원하시는 기능을 선택해주세요.");
 			System.out.println("0. 이전 페이지로 돌아가기");
@@ -170,15 +209,20 @@ public class RestaurantService {
 			if(select == 0) {
 				break;
 			} else if(select == 1) {
+				// 선택한 식당의 아이디를 가져옴!
 			    String wish_res_id = resDAO.selectWishRes(wish_n).getRes_id();
 			    
 			    WishlistDTO wishlistDTO = new WishlistDTO();
 			    
+			    // 로그인한 사용자의 id & 식당의 res_id를 wishlistDTO에 설정!
 			    wishlistDTO.setId(memberDTO.getId());
 			    wishlistDTO.setRes_id(wish_res_id);
 			    
 			    try {
 			    	
+			    	// 사용자가 이미 찜한 식당인지 확인하는 메소드.
+			    	// 이미 찜한 경우, "찜 목록에 이미 등록되어 있습니다!" 메시지 출력!
+			    	// 아니면, wishlistDAO.addWish(memberDTO.getId(), wish_res_id);를 호출 -> 찜 리스트에 추가
 			    	if (wishlistDAO.isAlreadyWished(wishlistDTO)) {
 			            System.out.println("찜 목록에 이미 등록되어 있습니다!");
 			        } else {
@@ -198,6 +242,9 @@ public class RestaurantService {
 		} // while
 	} // wishBestSelect()
 	
+	/*
+	 * 찜이 많이 된 순서에 따라 선택한 식당의 정보와 메뉴를 출력!
+	 */
 	// 찜 많은 순에서 원하는 식당 보기
 	public void selectWishRes(int select) {
 		printSpace();
